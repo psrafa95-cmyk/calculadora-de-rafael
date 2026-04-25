21a2dda03719dc8d3e30782c5094e9c376f106d5
Rafael Santana <psrafa95@gmail.com>
2026-04-25 16:32:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// Função que adiciona números ou operadores no display
 function add(valor){
-document.getElementById("display").value += valor;
+    document.getElementById("display").value += valor;
 }
 
+// Função que limpa o display
 function clearDisplay(){
-document.getElementById("display").value = "";
+    document.getElementById("display").value = "";
 }
 
+// Função que faz o cálculo final
 function calcular(){
-let resultado = document.getElementById("display").value;
-document.getElementById("display").value = eval(resultado);
+    let resultado = document.getElementById("display").value;
+    document.getElementById("display").value = eval(resultado);
 }
